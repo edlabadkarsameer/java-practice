8c9f76a5347dd1a117125cb86ab0ef5afcf1424a
Sameer Edlabadkar [Datamato] <sameer.edlabadkar@datamato.com>
2023-10-05 17:59:52 +0530

    src updated

--- src/pattern/ascendingTriangle.java
+++ src/pattern/ascendingTriangle.java
@@ -5,7 +5,7 @@ public class ascendingTriangle {
 		int a = 16;
 		for (int i = 0 ; i<a; i++) {
 			for (int j = 0; j<i; j++) {
-			System.out.print("*");
+			System.out.print("* ");
 		}
 			System.out.print("\n");
 		}
---
+++ src/pattern/decendingTriangle.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class decendingTriangle {
+	public static void main (String[] args) {
+		int a = 16;
+		for(int i = a; i>0; i--) {
+			for(int j = 0; j<i; j++) {
+				System.out.print("* ");
+			}
+			System.out.print("\n");
+		}
+		
+	}
+}
